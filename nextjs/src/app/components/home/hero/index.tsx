c88ea7900d1a4fa278121c573154a1a427c90684
dysk0zero <javiroldan98@gmail.com>
2025-08-04 18:59:07 +0200

    hotfix

--- nextjs/src/app/components/home/hero/index.tsx
+++ nextjs/src/app/components/home/hero/index.tsx
@@ -32,23 +32,25 @@ function Hero() {
         </div>
 
       <div 
-          className="lg:pt-[140px] lg:pl-[0px] md:pl-8 sm:pl-6 z-20">
-        <h5 className="
-            h-[280px] w-[680px] 
-            bg-[linear-gradient(120deg,_var(--background)_30%,_var(--mid-color)_60%,_var(--to-color)_90%)] 
-            rounded-4xl content-center  pl-8 
-            text-4xl md:text-5xl lg:text-[56px] font-semibold text-foreground leading-[1.65]
-            shadow-[-4px_4px_4px_rgba(208,255,244,0.5)]
-            text-shadow-hero"
+        className="lg:pt-[140px] lg:pl-[0px] md:pl-8 sm:pl-6 z-20"
+      >
+        <h1 className="sr-only">Javier Bellido Roldán - Portfolio Homepage</h1>
+        <h4 className="
+          h-[280px] w-[680px] 
+          bg-[linear-gradient(120deg,_var(--background)_30%,_var(--mid-color)_60%,_var(--to-color)_90%)] 
+          rounded-4xl content-center  pl-8 
+          text-4xl md:text-5xl lg:text-[56px] font-semibold text-foreground leading-[1.65]
+          shadow-[-4px_4px_4px_rgba(208,255,244,0.5)]
+          text-shadow-hero"
           style={headingStyles}
         >
           Decoding challenges, <br /> Designing futures
-        </h5>
+        </h4>
         <h2 className="
-            h-[200px] w-[680px] 
-            content-center text-right
-            text-2xl md:text-3xl lg:text-[32px] font-semibold text-foreground 
-            text-shadow-hero-sub"
+          h-[200px] w-[680px] 
+          content-center text-right
+          text-2xl md:text-3xl lg:text-[32px] font-semibold text-foreground 
+          text-shadow-hero-sub"
           style={headingStyles}
         >
           Physics-trained designer & developer <br /> helping businesses move faster.
